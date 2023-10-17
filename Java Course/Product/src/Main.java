98452d34df91d05e2f94b6d9a479f5a553a7162a
Emanuel Rocha <emanuelrocha831@gmail.com>
2023-10-17 11:10:49 -0300

    commiting Product with toString override

--- Java Course/Product/src/Main.java
+++ Java Course/Product/src/Main.java
@@ -7,8 +7,9 @@ public class Main {
     public static void main(String[] args) {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
-
         Product product  = new Product();
+        String string = product.toString();
+
         System.out.println("Enter product data:");
         System.out.println("Name: ");
         product.setName(sc.nextLine());
@@ -19,30 +20,18 @@ public class Main {
         System.out.println("Quantity in stock: ");
         product.setQuantity(sc.nextInt());
 
-        System.out.printf("Product data:%n");
-        System.out.printf("Name: %s%n", product.getName());
-        System.out.printf("Price: $ %.2f%n", product.getPrice());
-        System.out.printf("Quantity: %d units%n", product.getQuantity());
-        System.out.printf("Total: $ %.2f%n", product.TotalValueInStock());
-
+        System.out.println(string);
         System.out.println("Enter the number of products to be added in stock: ");
         product.AddProducts(sc.nextInt());
 
         System.out.printf("Updated data:%n");
-        System.out.printf("Name: %s%n", product.getName());
-        System.out.printf("Price: $ %.2f%n", product.getPrice());
-        System.out.printf("Quantity: %d units%n", product.getQuantity());
-        System.out.printf("Total: $ %.2f%n", product.TotalValueInStock());
-
+        System.out.println(string);
 
         System.out.println("Enter the number of products to be removed from stock: ");
         product.RemoveProducts(sc.nextInt());
 
         System.out.printf("Updated data:%n");
-        System.out.printf("Name: %s%n", product.getName());
-        System.out.printf("Price: $ %.2f%n", product.getPrice());
-        System.out.printf("Quantity: %d units%n", product.getQuantity());
-        System.out.printf("Total: $ %.2f%n", product.TotalValueInStock());
+        System.out.println(string);
 
 
 
--- Java Course/Product/src/entities/Product.java
+++ Java Course/Product/src/entities/Product.java
@@ -40,4 +40,13 @@ public class Product {
     public void setQuantity(int quantity) {
         this.quantity = quantity;
     }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "name='" + name + '\'' +
+                ", price=" + price +
+                ", quantity=" + quantity +
+                '}';
+    }
 }

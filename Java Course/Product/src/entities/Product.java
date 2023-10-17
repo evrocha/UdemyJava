36cf420f77f16ec7574064cb1b27ebaf70c9c5fa
Emanuel Rocha <emanuelrocha831@gmail.com>
2023-10-17 11:04:01 -0300

    commiting Product Program

---
+++ Java Course/Product/src/entities/Product.java
@@ -0,0 +1,43 @@
+package entities;
+public class Product {
+    private String name;
+    private double price;
+    private int quantity;
+
+    public double TotalValueInStock(){
+        return price * quantity;
+    }
+
+    public double AddProducts(int quantity){
+        return this.quantity += quantity;
+    }
+
+    public double RemoveProducts(int quantity){
+        return this.quantity -= quantity;
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+}
